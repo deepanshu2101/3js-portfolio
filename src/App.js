aef03ffab3f1855e5304228878f6a077fe466f2e
Deepanshu Bhardwaj <deepanshubhardwaj2101@gmail.com>
2023-07-27 22:25:10 +0530

    Add new files and initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layouts';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
